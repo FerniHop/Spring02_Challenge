5be94941a2ac70eaad0ab29c3dec1dac9f871363
Alexandre Gossard <119630621+FerniHop@users.noreply.github.com>
2023-10-11 08:57:03 +0200

    Add files via upload

---
+++ quest-spring-route/src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -0,0 +1,31 @@
+package com.wildcodeschool.doctor.controller;
+
+import com.wildcodeschool.doctor.model.Doctor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/doctor")
+public class DoctorController {
+
+    @GetMapping("/{number}")
+    @ResponseBody
+    public ResponseEntity<Object> getDoctorInfo(@PathVariable int number) {
+        if (number == 13) {
+            Doctor doctor = new Doctor(13, "Jodie Whittaker");
+            return ResponseEntity.ok(doctor);
+        } else if (number >= 1 && number <= 12) {
+            return ResponseEntity.status(HttpStatus.SEE_OTHER)
+                    .header("Location", "/")
+                    .build();
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Impossible de récupérer l'incarnation " + number + ".");
+        }
+    }
+}
